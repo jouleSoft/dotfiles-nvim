d4f0ef2bd1658a0cd044f8c677c2aa5d256880e8
Julio Jimenez Delgado <julio.jimenezdelgado@gmail.com>
2025-02-15 22:22:52 +0100

    New config

--- lua/config/plugins/neogit.lua
+++ lua/config/plugins/neogit.lua
@@ -7,9 +7,9 @@ return {
     "nvim-telescope/telescope.nvim", -- optional
   },
   config = function()
-    vim.keymap.set('n', '<leader>g.', '<cmd>Neogit<cr>', {desc = 'Open Neogit'})
+    vim.keymap.set('n', '<leader>og.', '<cmd>Neogit<cr>', { desc = 'Open Neogit' })
     require("neogit").setup({
-      kind="tab",
+      kind = "tab",
     })
   end,
 }
--- lua/config/plugins/which_key.lua
+++ lua/config/plugins/which_key.lua
@@ -1,24 +1,41 @@
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
+  dependencies = {
+    "echasnovski/mini.icons", --icons
+  },
   init = function()
     vim.o.timeout = true
     vim.o.timeoutlen = 300
   end,
   opts = function()
     require("which-key").register({
-      ["<leader><tab>"] = { name = "+Tabs" },
-      ["<leader>b"] = { name = "+Buffers" },
-      ["<leader>f"] = { name = "+File" },
-      ["<leader>g"] = { name = "+Git" },
-      ["<leader>h"] = { name = "+Help" },
-      ["<leader>l"] = { name = "+LSP" },
-      ["<leader>lf"] = { name = "+Find" },
-      ["<leader>n"] = { name = "+NvimTree" },
-      ["<leader>o"] = { name = "+Open" },
-      ["<leader>q"] = { name = "+Quit" },
-      ["<leader>s"] = { name = "+Search" },
-      ["<leader>w"] = { name = "+Window" },
+      -- ["<leader><tab>"] = { name = "+Tabs" },
+      -- ["<leader>b"] = { name = "+Buffers" },
+      -- ["<leader>f"] = { name = "+File" },
+      -- ["<leader>g"] = { name = "+Git" },
+      -- ["<leader>h"] = { name = "+Help" },
+      -- ["<leader>l"] = { name = "+LSP" },
+      -- ["<leader>lf"] = { name = "+Find" },
+      -- ["<leader>n"] = { name = "+NvimTree" },
+      -- ["<leader>o"] = { name = "+Open" },
+      -- ["<leader>q"] = { name = "+Quit" },
+      -- ["<leader>s"] = { name = "+Search" },
+      -- ["<leader>w"] = { name = "+Window" },
+
+      { "<leader><tab>", group = "Tabs",    desc = 'Tabs' },
+      { "<leader>b",     group = "Buffers" },
+      { "<leader>f",     group = "File" },
+      --{ "<leader>g",     group = "Git" },
+      { "<leader>h",     group = "Help" },
+      { "<leader>l",     group = "LSP" },
+      { "<leader>lf",    group = "Find" },
+      { "<leader>,",     group = "NvimTree" },
+      { "<leader>o",     group = "Open" },
+      { "<leader>og",    group = "NeoGit" },
+      { "<leader>q",     group = "Quit" },
+      { "<leader>s",     group = "Search" },
+      { "<leader>w",     group = "Window" },
     })
   end,
 }
