2acb4773387e0d79c7e6148297987b4bcea01174
Julio Jimenez Delgado <julio.jimenezdelgado@gmail.com>
2025-07-12 20:28:04 +0200

    updated

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -1,9 +1,10 @@
 return {
   'nvim-telescope/telescope.nvim',
-  tag = '0.1.5',
+  tag = '0.1.8',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' },
+    --{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' },
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release -DCMAKE_POLICY_VERSION_MINIMUM=3.5 && cmake --build build --config Release' },
     'nvim-tree/nvim-web-devicons',
   },
   config = function()
