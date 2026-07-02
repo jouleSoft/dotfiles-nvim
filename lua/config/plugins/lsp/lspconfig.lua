6af219706de9b103390a7859f92240d592d4345f
Julio Jimenez Delgado <julio.jimenezdelgado@gmail.com>
2026-07-02 18:47:02 +0200

    Modified lsp keymaps and deprecated functions

--- lua/config/plugins/lsp/lspconfig.lua
+++ lua/config/plugins/lsp/lspconfig.lua
@@ -13,43 +13,43 @@ return {
       local opts = { noremap = true, silent = true, buffer = bufnr }
 
       opts.desc = "Go to declaration"
-      keymap.set("n", "lD", vim.lsp.buf.declaration, opts)
+      keymap.set("n", "<leader>lD", vim.lsp.buf.declaration, opts)
 
       opts.desc = "See available code actions"
-      keymap.set({ "n", "v" }, "la", vim.lsp.buf.code_action, opts)
+      keymap.set({ "n", "v" }, "<leader>la", vim.lsp.buf.code_action, opts)
 
       opts.desc = "Show line diagnostics"
-      keymap.set("n", "le", vim.diagnostic.open_float, opts)
+      keymap.set("n", "<leader>le", vim.diagnostic.open_float, opts)
 
       opts.desc = "Smart rename"
-      keymap.set("n", "lr", vim.lsp.buf.rename, opts)
+      keymap.set("n", "<leader>lr", vim.lsp.buf.rename, opts)
 
       opts.desc = "Restart LSP"
-      keymap.set("n", "ls", ":LspRestart<CR>", opts)
+      keymap.set("n", "<leader>ls", ":LspRestart<CR>", opts)
 
       opts.desc = "Go to previous diagnostic"
-      keymap.set("n", "lh", vim.diagnostic.goto_prev, opts)
+      keymap.set("n", "<leader>lh", function() vim.diagnostic.jump({ count = -1, float = true }) end, opts)
 
       opts.desc = "Go to next diagnostic"
-      keymap.set("n", "ll", vim.diagnostic.goto_next, opts)
+      keymap.set("n", "<leader>lh", function() vim.diagnostic.jump({ count = 1, float = true }) end, opts)
 
       opts.desc = "Find LSP references"
-      keymap.set("n", "lfr", ":Telescope lsp_references<CR>", opts)
+      keymap.set("n", "<leader>lfr", ":Telescope lsp_references<CR>", opts)
 
       opts.desc = "Find LSP definitions"
-      keymap.set("n", "lfd", ":Telescope lsp_definitions<CR>", opts)
+      keymap.set("n", "<leader>lfd", ":Telescope lsp_definitions<CR>", opts)
 
       opts.desc = "Find LSP implementations"
-      keymap.set("n", "lfi", ":Telescope lsp_implementations<CR>", opts)
+      keymap.set("n", "<leader>lfi", ":Telescope lsp_implementations<CR>", opts)
 
       opts.desc = "Find LSP type definitions"
-      keymap.set("n", "lft", ":Telescope lsp_type_definitions<CR>", opts)
+      keymap.set("n", "<leader>lft", ":Telescope lsp_type_definitions<CR>", opts)
 
       opts.desc = "Find buffer diagnostics"
-      keymap.set("n", "lf.", ":Telescope diagnostics bufnr=0<CR>", opts)
+      keymap.set("n", "<leader>lf.", ":Telescope diagnostics bufnr=0<CR>", opts)
 
       opts.desc = "Show documentation for what is under cursor"
-      keymap.set("n", "K", vim.lsp.buf.hover, opts)
+      keymap.set("n", "<leader>lK", vim.lsp.buf.hover, opts)
     end
 
     local capabilities = cmp_nvim_lsp.default_capabilities()
